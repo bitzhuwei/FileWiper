3d7fc1b5308d13df86d4f2a7832a05b453a24977
bitzhuwei <bitzhuwei@qq.com>
2015-01-21 20:19:12 +0800

    basic wiping and deleting functions are done.

---
+++ FileWiper/FormMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FileWiper
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWipeFiles = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnUnregister = new System.Windows.Forms.Button();
+            this.openFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.btnDeleteFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnWipeFiles
+            // 
+            this.btnWipeFiles.Location = new System.Drawing.Point(12, 12);
+            this.btnWipeFiles.Name = "btnWipeFiles";
+            this.btnWipeFiles.Size = new System.Drawing.Size(228, 23);
+            this.btnWipeFiles.TabIndex = 0;
+            this.btnWipeFiles.Text = "Wipe Files";
+            this.btnWipeFiles.UseVisualStyleBackColor = true;
+            this.btnWipeFiles.Click += new System.EventHandler(this.btnWipeFiles_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(12, 70);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(228, 23);
+            this.btnRegister.TabIndex = 0;
+            this.btnRegister.Text = "Register for System Right Menu";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnUnregister
+            // 
+            this.btnUnregister.Location = new System.Drawing.Point(12, 99);
+            this.btnUnregister.Name = "btnUnregister";
+            this.btnUnregister.Size = new System.Drawing.Size(228, 23);
+            this.btnUnregister.TabIndex = 0;
+            this.btnUnregister.Text = "Unregister for System Right Menu";
+            this.btnUnregister.UseVisualStyleBackColor = true;
+            this.btnUnregister.Click += new System.EventHandler(this.btnUnregister_Click);
+            // 
+            // openFileDlg
+            // 
+            this.openFileDlg.Multiselect = true;
+            this.openFileDlg.Title = "Select files to wipe their contents";
+            // 
+            // btnDeleteFiles
+            // 
+            this.btnDeleteFiles.Location = new System.Drawing.Point(12, 41);
+            this.btnDeleteFiles.Name = "btnDeleteFiles";
+            this.btnDeleteFiles.Size = new System.Drawing.Size(228, 23);
+            this.btnDeleteFiles.TabIndex = 0;
+            this.btnDeleteFiles.Text = "Delete Files";
+            this.btnDeleteFiles.UseVisualStyleBackColor = true;
+            this.btnDeleteFiles.Click += new System.EventHandler(this.btnDeleteFiles_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 131);
+            this.Controls.Add(this.btnUnregister);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnDeleteFiles);
+            this.Controls.Add(this.btnWipeFiles);
+            this.Name = "FormMain";
+            this.Text = "File Wiper @ http://bitzhuwei.cnblogs.com";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWipeFiles;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnUnregister;
+        private System.Windows.Forms.OpenFileDialog openFileDlg;
+        private System.Windows.Forms.Button btnDeleteFiles;
+    }
+}
---
+++ FileWiper/FormMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileWiper
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWipeFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var count = 0;
+                foreach (var item in openFileDlg.FileNames)
+                {
+                    try
+                    {
+                        WipeFileContent(item);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                MessageBox.Show(string.Format("{0} file{1} content {2} successfully wiped!",
+                    count, count > 1 ? "s'" : "'s", count > 1 ? "are" : "is"));
+            }
+        }
+
+        private void WipeFileContent(string filename)
+        {
+            using (var stream = new System.IO.StreamWriter(filename, false))
+            {
+                stream.Write("http://bitzhuwei.cnblogs.com");
+            }
+        }
+
+        private void btnDeleteFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var count = 0;
+                foreach (var item in openFileDlg.FileNames)
+                {
+                    try
+                    {
+                        DeleteFile(item);
+                        count++;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                MessageBox.Show(string.Format("{0} file{1} {2} successfully deleted!",
+                    count, count > 1 ? "s" : "", count > 1 ? "are" : "is"));
+            }
+        }
+
+        private void DeleteFile(string filename)
+        {
+            var fileInfo = new System.IO.FileInfo(filename);
+            var directory = fileInfo.DirectoryName;
+            var name = 0;
+            var prefix = "";
+            var newFilename = System.IO.Path.Combine(directory, String.Format("{0}{1}", prefix, name));
+            while (System.IO.File.Exists(newFilename))
+            {
+                name++;
+                if (name == int.MaxValue)
+                {
+                    prefix += "x";
+                    name = 0;
+                }
+                newFilename = System.IO.Path.Combine(directory, String.Format("{0}{1}", prefix, name));
+            }
+
+            System.IO.File.Move(filename, newFilename);
+            System.IO.File.Delete(newFilename);
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUnregister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
