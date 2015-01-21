9a41e51ccc9b7af9d163c65d5e1f43920d410f59
bitzhuwei <bitzhuwei@qq.com>
2015-01-21 20:26:45 +0800

    Revert "changed form's width"
    This reverts commit c29b530e015f8504df7152296a6fd58bb506d042.

--- FileWiper/FormMain.Designer.cs
+++ FileWiper/FormMain.Designer.cs
@@ -84,7 +84,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 131);
+            this.ClientSize = new System.Drawing.Size(380, 131);
             this.Controls.Add(this.btnUnregister);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.btnDeleteFiles);
