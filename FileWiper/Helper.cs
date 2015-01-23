6e76caea8a680d83a92c88bb61fb47f4a2b43aa8
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-01-23 21:49:57 +0800

    Register "Wipe Content" to right menu of Explorer

---
+++ FileWiper/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileWiper
+{
+    class Helper
+    {
+        public static void WipeFileContent(string filename)
+        {
+            using (var stream = new System.IO.StreamWriter(filename, false))
+            {
+                stream.Write("http://bitzhuwei.cnblogs.com");
+            }
+        }
+    }
+}
